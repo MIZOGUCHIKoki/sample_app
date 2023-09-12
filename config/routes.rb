40c99e7197a8a69584621935eb464efd9c4fcb4f
MIZOGUCHI Koki <94270819+MIZOGUCHIKoki@users.noreply.github.com>
2023-09-12 21:24:04 +0900

    change route

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,14 @@ Rails.application.routes.draw do
   # static_pages はコントローラ名
   # roots / root （ルートと呼ぶ）
   # routes / route （ラウツと呼ぶ）
+  
+  # GET / => static_pages#home
+  root "static_pages#home" # root -> Home
+
+  # Get /static_pages/home => static_pages#home
   get 'static_pages/home'
   get 'static_pages/help'
-  get  'static_pages/about'
-  root "hello#index"
+  get 'static_pages/about'
+
+  # GET /about => static_pages#about
 end
