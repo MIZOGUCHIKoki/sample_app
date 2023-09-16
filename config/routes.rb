eaffa16f5b6f12b7d0108c04fda3534624ecc430
MIZOGUCHI Koki <94270819+MIZOGUCHIKoki@users.noreply.github.com>
2023-09-16 13:23:54 +0900

    Update

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'users/new'
   # static_pages はコントローラ名
   # roots / root （ルートと呼ぶ）
   # routes / route （ラウツと呼ぶ）
@@ -7,10 +8,14 @@ Rails.application.routes.draw do
   root "static_pages#home" # root -> Home
 
   # Get /static_pages/home => static_pages#home
-  get 'static_pages/home'
-  get 'static_pages/help'
-  get 'static_pages/about'
-  get 'static_pages/contact'
+  # get 'static_pages/home'
+  # get 'static_pages/help'
+  # get 'static_pages/about'
+  # get 'static_pages/contact'
 
   # GET /about => static_pages#about
+
+  get "/help", to: "static_pages#help"
+  get "/about", to: "static_pages#about"
+  get "/contact", to: "static_pages#contact"
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -8,7 +8,7 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   
   test "should get home" do
     # GET /static_pages/home
-    get static_pages_home_url
+    get root_path
     # RSP 200 〜なはずだ
     assert_response :success
     assert_select "title", "#{@base_title}"
@@ -16,21 +16,21 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
 
   test "should get help" do
     # GET /static_pages/help
-    get static_pages_help_url
+    get help_path
     # RSP 200 〜なはずだ
     assert_response :success
     assert_select "title", "Help | #{@base_title}"
   end
   
   test "should get about" do
-    get static_pages_about_url
+    get about_path
     assert_response :success
     assert_select "title", "About | #{@base_title}"
   end
 
   test "should get contact" do
     # GET /static_pages/help
-    get static_pages_contact_url
+    get contact_path
     assert_response :success
     assert_select "title", "Contact | #{@base_title}"
   end
