5e98b264988b7f58b2dd069f81b761e1cbd4a913
MIZOGUCHI Koki <94270819+MIZOGUCHIKoki@users.noreply.github.com>
2023-09-19 00:25:21 +0900

    Update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,19 @@
 class UsersController < ApplicationController
+# INDEX => SHOW => NEW => DESTROY
+
+# GET /users/:id
+  def show
+    @user = User.find(params[:id]) # パラメータSymbol
+    foo = "fooo"
+    bar = "barr"
+    # LOCAL Variable
+    # user = ... Instance Variable
+    
+    # => app/views/users/show.html.erb
+    # debugger
+  end
+
+# GET /users/new
   def new
   end
 end
